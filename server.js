1bef2e2946eb1d635b9d4ccec849917e6418b323
Muhamad Farid Ridho Rambe <muhamadfaridrr@gmail.com>
2021-04-26 22:48:15 +0700

    first-commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+const connectionString = "mongodb+srv://muhamadfaridrr:Farid1901!@cluster0.hzxkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    //Middleware
+
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+
+    //Routes
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req,res) => {
+        db.collection('quotes').find().toArray()
+         .then(results => {
+             res.render('index.ejs', {quotes:results})
+         })
+         .catch(error => console.error(error))
+    })
+
+    app.post('/quotes', (req,res) => {
+        quotesCollection.insertOne(req.body)
+         .then(result => {
+             res.redirect('/')
+         })
+         .catch(error => console.error(error))
+    })
+
+    app.put('/quotes', (req,res) =>{
+        quotesCollection.findOneAndUpdate(
+            { name: 'Darth Vadar' },
+            {
+                $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
+                }
+            },
+            {
+                upsert: true
+            }
+        )
+         .then(result => res.json('Success'))
+         .catch(error => console.error(error))
+    })
+
+    app.delete('/quotes', (req,res) => {
+        quotesCollection.deleteOne(
+            { name: req.body.name }
+        )
+         .then(result => {
+             if(result.deletedCount === 0 ){
+                 return res.json('No quote to delete')
+             }
+             res.json('Deleted Darth Vadar"s quote')
+         })
+         .catch(error => console.error(error))
+    })
+
+    app.listen(3000, function(){
+        console.log('listening on 3000')
+    })
+  })
